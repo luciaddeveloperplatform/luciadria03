e7d44f10ca7d1b6855979b6406ba1fc4124b3811
Luciad Developer Platform <60262140+luciaddeveloperplatform@users.noreply.github.com>
2020-02-11 20:50:32 +0100

    Adding missing folder

---
+++ src/factories/ModelFactory.js
@@ -0,0 +1,38 @@
+import { LonLatGrid } from "@luciad/ria/view/grid/LonLatGrid";
+import { WMSTileSetModel } from "@luciad/ria/model/tileset/WMSTileSetModel";
+import {FusionTileSetModel} from "@luciad/ria/model/tileset/FusionTileSetModel";
+import {createBounds} from "@luciad/ria/shape/ShapeFactory";
+import { getReference } from "@luciad/ria/reference/ReferenceProvider";
+import { UrlTileSetModel } from "@luciad/ria/model/tileset/UrlTileSetModel";
+import {WMTSTileSetModel} from "@luciad/ria/model/tileset/WMTSTileSetModel";
+
+class ModelFactory {
+    // Creates a Grid Model
+    static createGridModel() {
+        return new LonLatGrid();
+    }
+
+    // Creates a WMS Model
+    static createWMSModel(options) {
+        return new WMSTileSetModel(options);
+    }
+
+    // Creates a LTS Model
+    static createLTSModel (options) {
+        return new FusionTileSetModel(options)
+    }
+
+    // Creates a TMS Model
+    static createTMSModel(options) {
+        const REF_WEBMERCATOR = getReference("EPSG:3857");
+        // Most TMS services use WebMercator so we can set it as default
+        options.reference = options.reference ? options.reference : REF_WEBMERCATOR;
+        // Most TMS services cover the entire world in WebMercator bounds, so we set it as default
+        options.bounds = options.bounds ?
+            options.bounds :
+            createBounds(REF_WEBMERCATOR, [-20037508.34278924, 40075016.68557848, -20037508.3520, 40075016.7040]);
+        return new UrlTileSetModel(options);
+    }
+}
+
+export default ModelFactory;
